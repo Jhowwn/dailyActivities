bc02da22bf465299d2b2d5b0f41bb654023df6fc
LetsWorkb1 <jhow.win@gmail.com>
2021-11-19 11:01:59 -0300

    Adicionar arquivos de projeto.

---
+++ dailyActivities/dailyActivities/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dailyActivities.Helpers
+{
+    class SQLiteDataBaseHelper
+    {
+    }
+}
---
+++ dailyActivities/dailyActivities/Models/Atividade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace dailyActivities.Models
+{
+    public class Atividade
+    {
+        [primarykey, AutoIncreament]
+        public int id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public double? Peso { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
